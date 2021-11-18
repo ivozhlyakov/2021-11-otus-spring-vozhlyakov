c382ea6c82fb650a06b6b6ec8006aefe12b5aebb
ivozhlyakov <ivozhlyakov@diasoft.ru>
2021-11-19 00:45:49 +0300

    exercise 2 complete

--- springStudTest/src/main/java/ru/ivozhlyakov/springStudTest/service/QuestionService.java
+++ springStudTest/src/main/java/ru/ivozhlyakov/springStudTest/service/QuestionService.java
@@ -1,18 +1,29 @@
 package ru.ivozhlyakov.springStudTest.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
 import ru.ivozhlyakov.springStudTest.dao.QuestionDAO;
 import ru.ivozhlyakov.springStudTest.domain.Answer;
 import ru.ivozhlyakov.springStudTest.domain.Question;
+import ru.ivozhlyakov.springStudTest.domain.TestResult;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Scanner;
 
-
+@Service
 public class QuestionService {
 
     private QuestionDAO questionDAO;
+    private TestResult testResult;
+
+    @Value("${result.pass}")
+    private String pass;
+
+    @Value("${result.failed}")
+    private String failed;
 
     public QuestionService(QuestionDAO questionDAO) {
         this.questionDAO = questionDAO;
@@ -22,27 +33,49 @@ public class QuestionService {
        return questionDAO.getQuestionList();
    }
 
-    public void loadQuestionList() {
-        try {
-            questionDAO.loadObjectList();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private Integer getCountToPassExam(){
+        return questionDAO.getCountToPassExam();
     }
 
-    public void setResource(Resource resource){
-        questionDAO.setResource(resource);
-    }
+    public void test() {
+        this.testResult = new TestResult();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your family name: ");
+        this.testResult.setFamilyName(scanner.next());
 
-    public void print() {
+        System.out.print("Enter your name: ");
+        this.testResult.setName(scanner.next());
         for (Question question : getQuestionList()) {
             StringBuilder builder = new StringBuilder(question.getValue());
             int i = 1;
+            builder.append("\n");
             for (Answer answer : question.getAnswerList()) {
-                builder.append("\n").append(i).append(". ").append(answer.getValue());
+                builder.append(i).append(". ").append(answer.getValue()).append("    ");
                 i += 1;
             }
             System.out.println(builder.toString());
+            String answer = scanner.next();
+
+            if (answer != null
+                    && question.getAnswerList()
+                    .stream()
+                    .filter(a -> a.isCorrect())
+                    .findAny().get()
+                    .getValue().equals(answer))
+            {
+                this.testResult.addCorrectAnswer();
+            }
         }
+
+        checkResult();
+    }
+
+    private void checkResult() {
+        String result = failed;
+        if (testResult.getCorrectAnswerCount()
+                .compareTo(getCountToPassExam()) > -1){
+         result = pass;
+        }
+        System.out.println(result);
     }
 }
