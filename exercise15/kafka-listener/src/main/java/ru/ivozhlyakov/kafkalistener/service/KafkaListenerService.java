6d5f6638397445e901fcc7b5e71aa0942e5445cc
ivozhlyakov <ivozhlyakov@diasoft.ru>
2022-03-14 11:46:32 +0300

    4.4 Messaging-системы, Kafka

---
+++ exercise15/kafka-listener/src/main/java/ru/ivozhlyakov/kafkalistener/service/KafkaListenerService.java
@@ -0,0 +1,5 @@
+package ru.ivozhlyakov.kafkalistener.service;
+
+public interface KafkaListenerService {
+    void listenGroupFoo(String message);
+}
---
+++ exercise15/kafka-server/src/main/java/ru/ivozhlyakov/kafkaserver/KafkaApplication.java
@@ -0,0 +1,16 @@
+package ru.ivozhlyakov.kafkaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApplication.class, args);
+
+	}
+
+
+}
